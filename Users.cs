a34e63a19bacd3265994532d48d1b8788cc546c9
Ramuajay <ramuajaychandra@gmail.com>
2021-01-17 16:02:13 -0500

    Add files via upload

---
+++ Users.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RegistrationApp
+{
+    public class Users
+    {
+
+        public int UserID;
+        public string username;
+
+        
+        public void CreateNewUser(Users u)
+        {
+
+            ///CREATE NEW USER C# and SQL CODE
+            ///
+        }
+        public int ModifyUser(Users u)
+        {
+
+            return 1;
+
+        }
+
+        public void DeleteUser(Users u)
+        {
+
+
+
+        }
+
+        public void InactivateUser(Users u)
+        {
+
+
+
+        }
+    }
+}
